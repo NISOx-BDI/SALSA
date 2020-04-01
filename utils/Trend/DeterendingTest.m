17498b58e6c6d1dba3eb6a93b0dfc743ccb5efd5
asoroosh <srafyouni@gmail.com>
2020-04-01 10:51:50 +0100

    dct added

--- utils/Trend/DeterendingTest.m
+++ utils/Trend/DeterendingTest.m
@@ -1,6 +1,6 @@
 clear
 
-t  = 0:200;
+t  = 0:500;
 TR = 2;
 LowFreq  = sin(2*pi*(1/250)*t);
 HighFreq = 3*sin(2*pi*(1/10)*t);
@@ -23,11 +23,25 @@ Yt4 = Y - polyval(polyfit(t,Y,pnum),t);
 filter=hp_fsl(T,100,TR);
 Yft0 = filter*Y';
 
+SPMdefaultHPCutOff = 128;
+% built-in SPM
+%K = struct('RT',TR,'row',1:T,'HParam',SPMdefaultHPCutOff);
+%Ydct = spm_filter(K,Y'); %DCT is happenening withing the function 
+% Similar:
+DCT_pnum  = fix(2*(T*TR)/SPMdefaultHPCutOff + 1);
+DCT_hpf   = spm_dctmtx(T,DCT_pnum);
+dctY = Y' - DCT_hpf*(DCT_hpf'*Y');
+
 figure; hold on; 
 plot(t,Y,'LineWidth',1.5,'LineStyle','-.')
-plot(t,Yt2)
-plot(t,Yft0)
+plot(t,Yt2,'LineWidth',1.5,'LineStyle','-.','Color','k')
+plot(t,Yft0,'LineWidth',1.5,'LineStyle','-.','Color','b')
 plot(t,Yt3)
 plot(t,Yt4)
-legend('raw','poly','hpf','matlab','polyfitmatlabfunc')
+plot(t,dctY,'LineWidth',1.5,'LineStyle','-.','Color','r')
+legend('raw','poly','hpf','matlab','polyfitmatlabfunc','DCT')
 %plot(t,x-y)
+
+mean((HighFreq'-dctY).^2)
+mean((HighFreq-Yft0').^2)
+mean((HighFreq-Yt2).^2)
