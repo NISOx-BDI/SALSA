55a0bb977daf5c0fd80b11b0df2bcc42b5b61d86
asoroosh <srafyouni@gmail.com>
2020-04-02 15:11:41 +0100

    1/f noise added

--- utils/Trend/DeterendingTest.m
+++ utils/Trend/DeterendingTest.m
@@ -1,28 +1,37 @@
 clear
 
-t  = 0:500;
+T = 500; 
+
+t  = 1:T;
 TR = 2;
-LowFreq  = sin(2*pi*(1/250)*t);
-HighFreq = 3*sin(2*pi*(1/10)*t);
-DC = 3; 
 
-Y = DC + HighFreq + LowFreq;
-T = numel(Y);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Make 1/f noise %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% similar to AFNI
+% copied from: https://uk.mathworks.com/matlabcentral/answers/344786-generation-of-1-f-noise-using-matlab
+fv = linspace(0, 1, 20);                                % Normalised Frequencies
+a = 1./(1 + fv*2);                                      % Amplitudes Of ?1/f?
+b = firls(42, fv, a);                                   % Filter Numerator Coefficients                                     % Filter Bode Plot
+ns = rand(1, T);
+invfn = filtfilt(b, 1, ns);                             % Create ?1/f? Noise
+
+Y = invfn; %+ LowFreq(1:T);
+Y = Y - mean(Y); % just demean so we can see the results easier.  
+% similar to AFNI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 pnum = 1+floor(TR.*T/150);
-Yt2 = multpolyfit(repmat(1:T,1,1)',Y',T,pnum);
-
-% matlab easy function -- sanity check
-Yt3 = detrend(Y,pnum); % this is useless because only assumes linear in my current matlab version (R2016b)
+pnum = 3;
+Yt2 = multpolyfit(repmat(1:T,1,1),Y,T,pnum);
 
 %Just to check whether xt3 matches the matlab functions
 Yt4 = Y - polyval(polyfit(t,Y,pnum),t);
 
-% FSL what does
-filter=hp_fsl(T,100,TR);
-Yft0 = filter*Y';
+% FSL what does %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+filter = hp_fsl(T,100,TR);
+Yft0   = filter*Y';
 
+
+% SPM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 SPMdefaultHPCutOff = 128;
 % built-in SPM
 %K = struct('RT',TR,'row',1:T,'HParam',SPMdefaultHPCutOff);
@@ -32,16 +41,18 @@ DCT_pnum  = fix(2*(T*TR)/SPMdefaultHPCutOff + 1);
 DCT_hpf   = spm_dctmtx(T,DCT_pnum);
 dctY = Y' - DCT_hpf*(DCT_hpf'*Y');
 
+
+% Plot %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure; hold on; 
 plot(t,Y,'LineWidth',1.5,'LineStyle','-.')
 plot(t,Yt2,'LineWidth',1.5,'LineStyle','-.','Color','k')
 plot(t,Yft0,'LineWidth',1.5,'LineStyle','-.','Color','b')
-plot(t,Yt3)
 plot(t,Yt4)
 plot(t,dctY,'LineWidth',1.5,'LineStyle','-.','Color','r')
-legend('raw','poly','hpf','matlab','polyfitmatlabfunc','DCT')
+legend('raw','poly','hpf','polyfitmatlabfunc','DCT')
 %plot(t,x-y)
 
-mean((HighFreq'-dctY).^2)
-mean((HighFreq-Yft0').^2)
-mean((HighFreq-Yt2).^2)
+% MSE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% mean((HighFreq'-dctY).^2)
+% mean((HighFreq-Yft0').^2)
+% mean((HighFreq-Yt2).^2)
