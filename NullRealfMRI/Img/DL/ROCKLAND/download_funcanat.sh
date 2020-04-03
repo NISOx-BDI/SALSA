7cb81db527d539e4e8ca4ad3631e38c675490a7b
asoroosh <srafyouni@gmail.com>
2020-04-03 09:40:53 +0100

    script to download func and anat

---
+++ NullRealfMRI/Img/DL/ROCKLAND/download_funcanat.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+module load Python/3.6.6-foss-2018b
+
+CohortID=ROCKLAND
+outp="/well/nichols/users/scf915/${CohortID}"
+
+python3 download_rockland_raw_bids.py -o $outp -e REST645 -t func,anat -v DS2
+
