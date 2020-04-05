36f50d995c40d686128ebe9653b349feb85d1b60
asoroosh <srafyouni@gmail.com>
2020-04-05 10:53:16 +0100

    OLS done ARMA and the main bit remained

---
+++ mis/GenerateED.m
@@ -0,0 +1,15 @@
+
+function EDX = GenerateED(lD,T,TR)
+% lD: length of stimuli in seconds
+% T : length of the scan
+% TR: TR in second
+
+    BCWidth     = fix(lD/2/TR);
+    oneSTIM     = [zeros(1, BCWidth), ones(1, BCWidth)];
+    numberSTIM  = fix(T/numel(oneSTIM));
+    DD          = repmat(oneSTIM, [1, numberSTIM]);
+    hrf         = spm_hrf(TR); 
+    conv_dsgn   = conv(DD,hrf); 
+    EDX         = conv_dsgn(1:T)';
+
+end
--- mis/myOLS.m
+++ mis/myOLS.m
@@ -1,15 +1,44 @@
-function [Bhat,Yhat,res,se] = myOLS(Y,X)
+function [cBhat,Yhat,res,stat] = myOLS(Y,X,contrast)
 
+    % scalar: lower-case
+    % vector/matrix: capital
+    
     Y = Y(:); % make it a column 
-    T = numel(Y); 
+    t = numel(Y); 
 
-    if size(X,1) ~= T; X = X'; end
+    if size(X,1) ~= t; X = X'; end
 
-    Bhat  = X\Y; % \Beta = X^{+}Y
-    Yhat  = X*Bhat; %
-    res   = Y-Yhat; % sanitycheck: d.resid(1:5)
-    COVX  = (X'*X);
-    df    = T-rank(X);
-    s2    = (res'*res)./df;
-    se    = sqrt(s2./diag(COVX));
+    p           = rank(X);
+    stat.Bhat   = X\Y; % \Beta = X^{+}Y
+    Yhat        = X*stat.Bhat; %
+    res         = Y-Yhat; % sanitycheck: d.resid(1:5)
+    COVX        = (X'*X);
+    stat.df     = t-p;
+    
+    
+    if ~exist('contrast','var')
+        contrast    = zeros(1,p);
+        contrast(2) = 1;
+    end
+        
+    stat.mse   = sum(res.^2)/(t-p); 
+    stat.se    = sqrt(stat.mse*(contrast*pinv(COVX)*contrast'));
+    
+    cBhat      = contrast*stat.Bhat;
+    stat.tval  = cBhat./stat.se;
+    stat.pval  = 2*tcdf(-abs(stat.tval),stat.df);
+    
+    rss = sum(res.^2);
+    % these two don't match what Matlab's fitglm.m outputs
+    stat.AIC   = 2*(p+1)+t*log(rss/t);
+    stat.BIC   = log(t)*(p+1)+t*log(sum(res.^2)/t);
+    
 end
+
+
+
+
+% WOW~
+% beautiful for teaching: 
+% http://www.karenkopecky.net/Teaching/eco613614/Matlab%20Resources/OLS.pdf
+
