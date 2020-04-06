07277a39d0c1a6f93a37cd60c5d79d0e1cb5de62
asoroosh <srafyouni@gmail.com>
2020-04-06 19:56:06 +0100

    tcdf doesnt exist in the current octave vrs

--- mis/myOLS.m
+++ mis/myOLS.m
@@ -26,7 +26,7 @@ function [cBhat,Yhat,res,stat] = myOLS(Y,X,contrast)
     
     cBhat      = contrast*stat.Bhat;
     stat.tval  = cBhat./stat.se;
-    stat.pval  = 2*tcdf(-abs(stat.tval),stat.df);
+    stat.pval  = 2*myOLS_tcdf(-abs(stat.tval),stat.df);
     
     rss = sum(res.^2);
     % these two don't match what Matlab's fitglm.m outputs
@@ -36,7 +36,41 @@ function [cBhat,Yhat,res,stat] = myOLS(Y,X,contrast)
 end
 
 
+function p = myOLS_tcdf(x,n);
+% TCDF returns student cumulative distribtion function
+%
+% cdf = tcdf(x,DF);
+%
 
+% check size of arguments
+if all(size(x)==1)
+        x = repmat(x,size(n));
+elseif all(size(n)==1)
+        n = repmat(n,size(x));
+elseif all(size(x)==size(n))
+        ;	%% OK, do nothing
+else
+    	error('size of input arguments must be equal or scalar')
+end;
+
+% allocate memory
+p = zeros(size(x));
+p((x==Inf) & (n>0)) = 1;
+
+% workaround for invalid arguments in BETAINC
+ix   = isnan(x) | ~(n>0);
+p(ix)= NaN;
+
+ix    = (x > -Inf) & (x < Inf) & (n > 0);
+p(ix) = betainc (n(ix) ./ (n(ix) + x(ix).^2), n(ix)/2, 1/2) / 2;
+
+ix    = find(x>0);
+p(ix) = 1 - p(ix);
+
+% shape output
+p = reshape(p,size(x));
+
+end
 
 % WOW~
 % beautiful for teaching: 
