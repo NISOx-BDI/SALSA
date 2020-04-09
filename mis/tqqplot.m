f623eb526f8f46e09d0ff0c21f236a01607f52bf
asoroosh <srafyouni@gmail.com>
2020-04-09 11:01:45 +0100

    QQplot for tstats

---
+++ mis/tqqplot.m
@@ -0,0 +1,8 @@
+function [x,y] = tqqplot(tstat,df)
+
+tstat   = sort(tstat);
+np      = 1000;
+p       = (0+1/(np-1) : 1/(np-1) : 1-1/(np-1))';
+p(1:5)
+x       = tinv(p,df);
+y       = quantile(tstat,p);
