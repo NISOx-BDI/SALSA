b4b3e3c5924e5c6b160739b5f925fe4fe019de66
asoroosh <srafyouni@gmail.com>
2020-04-15 18:11:01 +0100

    pd not spd

--- mis/makeSPD.m
+++ mis/makeSPD.m
@@ -2,9 +2,10 @@ function K = makeSPD(K)
 
     [U,D] = eig(K);   % calculate the eigenvalues and eigenvectors of the GRM
     if min(diag(D)) < 0   % check whether the GRM is non-negative definite
-        disp('WARNING: the GRM is not non-negative definite! Set negative eigenvalues to zero')
-        D(D<0) = 0;   % set negative eigenvalues to zero
+        disp('makeSPD:: the matrix is not non-negative definite! Set negative eigenvalues to zero')
+        D(D<0) = eps;   % set negative eigenvalues to zero
         K = U*D/U;   % reconstruct the GRM
     end
+    K = (K + K')./2;
 
 end
