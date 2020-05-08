39f17fb43b1b8975959dd733d8855cf311ff44f8
asoroosh <srafyouni@gmail.com>
2020-05-08 20:33:49 +0100

    ROCKLAND submitter

--- NullRealfMRI/Img/Submit_NullImgSim_ROCKLAND.sh
+++ NullRealfMRI/Img/Submit_NullImgSim_ROCKLAND.sh
@@ -1,15 +1,19 @@
 #bin/bash
 
-
-NCORES=3
+NCORES=2
 COHORT=ROCKLAND
 
-METHODLIST=(ACF AR-YW AR-W ARMAHR)
+T=120 #
+TRs=2.5 # in second
+
+TR=$(echo $TRs*1000 | bc | awk -F'.' {'print $1'})
+
+METHODLIST=(ACF ACFadj AR-W ARMAHR)
 ARMODE=(1 2 5 10 20)
-ACMODE=(5 10 15 30 60)
+ACMODE=(5 10 15 $(echo "sqrt($T)" | bc) $(echo "2*sqrt($T)" | bc))
 
 FWHMsize=0
-TempTreMethod="hpf"
+TempTreMethod="dct"
 
 STRGDIR=/well/nichols/users/scf915
 COHORTDIR=${STRGDIR}/${COHORT}
@@ -17,12 +21,12 @@ COHORTDIR=${STRGDIR}/${COHORT}
 Path2ImgRaw=${STRGDIR}/${COHORT}/raw
 
 ############################################
-TR=$(cat ${Path2ImgRaw}/task-rest_acq-645_bold.json | grep RepetitionTime | awk {'print $2'} | awk -F"," '{print $1}')
+#TR=$(cat ${Path2ImgRaw}/task-rest_acq-645_bold.json | grep RepetitionTime | awk {'print $2'} | awk -F"," '{print $1}')
 SesID=DS2
 NUMJB=$(cat ${COHORTDIR}/sub.txt | wc -l )
 ############################################
 
-SUBMITDIR=/users/nichols/scf915/bin/FILM2/NullRealfMRI/Img/${COHORT}_Submitters
+SUBMITDIR=/users/nichols/scf915/bin/FILM2/NullRealfMRI/Img/${COHORT}_${T}_${TR}_Submitters
 mkdir -p ${SUBMITDIR}
 
 for METH_ID in ${METHODLIST[@]}
@@ -31,12 +35,14 @@ do
 
 	MAO=0; ARMODE0=${ARMODE[@]}
 	[ $METH_ID == "ARMAHR" ]&& MAO=1
-	[ $METH_ID == "ACF" ]&& ARMODE0=${ACMODE[@]}
+	if [ $METH_ID == "ACF" ] || [ $METH_ID == "ACFadj" ]; then
+		ARMODE0=${ACMODE[@]}
+	fi
 
 	for ARO in ${ARMODE0[@]}
 	do
 
-		JobName=${COHORT}_${METH_ID}_AR-${ARO}_MA-${MAO}_FWHM${FWHMsize}_${TempTreMethod}
+		JobName=${COHORT}_${TR}_${T}_${METH_ID}_AR-${ARO}_MA-${MAO}_FWHM${FWHMsize}_${TempTreMethod}
 		SubmitterFileName="${SUBMITDIR}/SubmitMe_${JobName}.sh"
 
 		echo ${SubmitterFileName}
@@ -56,7 +62,7 @@ cat > $SubmitterFileName << EOF
 #$ -o ${OpLog}/${JobName}_\\\$JOB_ID_\\\$TASK_ID.out
 #$ -e ${OpLog}/${JobName}_\\\$JOB_ID_\\\$TASK_ID.err
 #$ -N ${JobName}
-#$ -t 1-50 #${NUMJB}
+#$ -t 1-51 #${NUMJB}
 
 export OMP_NUM_THREADS=${NCORES}
 
@@ -70,12 +76,13 @@ echo 0 > \$STATFILE
 # module use -a /apps/eb/skylake/modules/all
 module load Octave/4.4.1-foss-2018b
 
-SubID=\$(cat ${COHORTDIR}/sub.txt | sed "\${SGE_TASK_ID}q;d" )
-SubID=\$(echo \${SubID} | awk -F"-" '{print \$2}')
+#SubID=\$(cat ${COHORTDIR}/sub.txt | sed "\${SGE_TASK_ID}q;d" )
+SubID=\$(cat ${COHORTDIR}/participants.tsv | awk {'print \$1'} | sed "\${SGE_TASK_ID}q;d")
+#SubID=\$(echo \${SubID} | awk -F"-" '{print \$2}')
 
 OCTSCRPT=\${HOME}/bin/FILM2/NullRealfMRI/Img
 cd \${OCTSCRPT}
-octave -q --eval "COHORTDIR=\"${COHORTDIR}\"; Path2ImgResults=\"${Path2ImgResults}\"; pwdmethod=\"${METH_ID}\"; lFWHM=${FWHMsize}; TR=${TR}; Mord=${ARO}; MPparamNum=${MAO}; TempTreMethod=\"${TempTreMethod}\"; SubID=\"\${SubID}\"; SesID=\"${SesID}\"; NullSim_Img_bmrc; quit"
+octave -q --eval "COHORTDIR=\"${COHORTDIR}\"; Path2ImgResults=\"${Path2ImgResults}\"; pwdmethod=\"${METH_ID}\"; lFWHM=${FWHMsize}; TR=${TRs}; Mord=${ARO}; MPparamNum=${MAO}; TempTreMethod=\"${TempTreMethod}\"; SubID=\"\${SubID}\"; SesID=\"${SesID}\"; NullSim_Img_bmrc; quit"
 
 # The stat file
 echo 1 > \$STATFILE
