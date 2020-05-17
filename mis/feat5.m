2f9578955741b695de49104cf102f65df04b1a03
asoroosh <srafyouni@gmail.com>
2020-05-17 15:34:18 +0100

    housekeeping

--- mis/feat5.m
+++ mis/feat5.m
@@ -24,7 +24,7 @@
 % X     = hp_ff*X;    % high pass filter the design
 % Y     = hp_ff*Y;  % high pass filter the data
 % 
-% [cbhat,Yhat,RES,stat,Wcbhat,WYhat,WRES] = feat5(Y,X,tcon,ImgStat,path2mask);
+% [cbhat,RES,stat,se,tv,zv,Wcbhat,WYhat,WRES,wse,wtv,wzv] = feat(Y,X,tcon,[],ImgStat,path2mask,1);
 % 
 % [PSDx,PSDy]   = DrawMeSpectrum(RES,1);
 % [WPSDx,WPSDy] = DrawMeSpectrum(WRES,1);
@@ -34,58 +34,100 @@
 % plot(WPSDx,mean(WPSDy,2))
 
 
-function [cbhat,Yhat,RES,stat,Wcbhat,WYhat,WRES] = feat5(Y,X,tcon,ImgStat,path2mask)
-% Y    : TxV
-% X    : TxEV
-% tcon : 1xEV
+function [cbhat,RES,stat,se,tv,zv,Wcbhat,WYhat,WRES,wse,wtv,wzv] = feat5(Y,X,tcon,tukey_m,ImgStat,path2mask,badjflag)
+% Y      : TxV
+% X      : TxEV
+% tcon   : 1xEV
+% tukey_m: int
+%
 % Y should gone through detrending & smoothing prior to this function.
 %
+% ${FSLDIR}/src/feat5/prewhiten.cc
 % ${FSLDIR}/src/feat5/featlib.cc
+%
 % SA, Ox, 2020
 
-ntp                   = size(Y,1); 
-nvox                  = size(Y,2); 
-[cbhat,Yhat,RES,stat] = myOLS(Y,X,tcon); % if cbhat is not needed; R=I-(XX+); RES=RY
+    if ~exist('badjflag','var'); badjflag = 0; end;
 
-% calc acf & tukey taper it
-tukey_m     = round(sqrt(ntp));
-acf_tukey   = acf_prep(RES,tukey_m);
+    ntp                   = size(Y,1); 
+    nvox                  = size(Y,2); 
+    [cbhat,~,~,stat] = myOLS(Y,X,tcon); % not really needed unless for comparison
 
-% smooth ACF
-FWHMl       = 5; 
-acf_tukey   = ApplyFSLSmoothing(acf_tukey',FWHMl,ImgStat,path2mask)';
-% make the pwfilter
-W_fft       = establish_pwfilter(acf_tukey,ntp);
 
-% Prewhiten the time series
-WY          = prewhiten_timeseries(Y,W_fft);
+    se                   = stat.se;
+    tv                   = stat.tval;
+    zv                   = stat.zval;
 
-% Prewhiten the design
-X_fft       = fft_model(X);
+    
+    pinvX               = pinv(X); 
+    ResidFormingMat     = eye(ntp)-X*pinvX; % residual forming matrix 
+    RES                 = ResidFormingMat*Y;    
+    
+    if ~badjflag; ResidFormingMat = [];  end
+    
+    % calc acf & tukey taper it
+    if isempty(tukey_m); tukey_m = round(sqrt(ntp)); end
+    
+    acf_tukey   = acf_prep(RES,tukey_m,ResidFormingMat);
 
-% refit the model to pre-whitened data
-Wcbhat = zeros(1,nvox);
-WYhat  = zeros(ntp,nvox); 
-WRES   = zeros(ntp,nvox);
-for iv = 1:nvox
-    if ~mod(iv,5000); disp(['on voxel: ' num2str(iv)]); end; 
-    WX                        = prewhiten_model(X_fft,W_fft(:,iv),ntp);
-    [Wcbhat(iv),WYhat(:,iv),WRES(:,iv)] = myOLS(WY(:,iv),WX,tcon);
+    % smooth ACF
+    FWHMl       = 5; 
+    acf_tukey   = ApplyFSLSmoothing(acf_tukey',FWHMl,ImgStat,path2mask)';
+    % make the pwfilter
+    W_fft       = establish_pwfilter(acf_tukey,ntp);
+
+    % Prewhiten the time series
+    WY          = prewhiten_timeseries(Y,W_fft);
+
+    % Prewhiten the design
+    X_fft       = fft_model(X);
+
+    % refit the model to pre-whitened data
+    Wcbhat = zeros(1,nvox);
+    WYhat  = zeros(ntp,nvox); 
+    WRES   = zeros(ntp,nvox);
+    wse    = zeros(nvox,1); 
+    wtv    = zeros(nvox,1);
+    wzv    = zeros(nvox,1);
+    for iv = 1:nvox
+        if ~mod(iv,5000); disp(['on voxel: ' num2str(iv)]); end; 
+        WX                        = prewhiten_model(X_fft,W_fft(:,iv),ntp);
+        [Wcbhat(iv),WYhat(:,iv),WRES(:,iv),wstat] = myOLS(WY(:,iv),WX,tcon);
+
+        wse(iv)  = wstat.se;
+        wtv(iv)  = wstat.tval;
+        wzv(iv)  = wstat.zval;    
+
+    end
+
 end
 
-end
-
-function acf_tukey = acf_prep(RES,tukey_m)
+function acf_tukey = acf_prep(RES,tukey_m,ResidFormingMat)
 % RES should be TxV
     
-    ntp         = size(RES,1);
-    acf         = AC_fft(RES,ntp)'; 
-    nvox        = size(acf,2); 
-    acf         = acf(1:tukey_m,:); 
+    [ntp,nvox]  = size(RES);
+    [~,~,acv]   = AC_fft(RES,ntp);
+    acv         = acv';
+    
+    % adjust for bias
+    if nargin==3 || isempty(ResidFormingMat)
+        BiasAdj = ACFBiasAdjMat(ResidFormingMat,ntp,tukey_m);
+        disp('feat:: adjusting autocovariances.')
+    elseif nargin<3
+        BiasAdj = eye(tukey_m+1); 
+    end
+    
+    
+    acv         = BiasAdj*acv(1:tukey_m+1,:); %apply adjustment
+    acf         = acv./acv(1,:); % get ACF
+    
+    
+    % Tukey taper
+    acf         = acf(1:tukey_m,:);
     lag         = 0:tukey_m-1;
     window      = .5 * (1 + cos(pi .* lag ./ tukey_m));
     acf_tukey   = acf .* repmat(window',1,nvox);
-        
+    
 end
 
 function W_fft = establish_pwfilter(acf,ntp)
@@ -136,6 +178,21 @@ function WX = prewhiten_model(X_fft,W_fft_v,ntp)
         WX0          = real(ifft((W_fft_v.*real(X_fft_ev)+W_fft_v.*imag(X_fft_ev)*1j),z_pad,1));
         WX(:,ev_cnt) = WX0(1:ntp,:);
     end
+end
+
+function invM_biasred = ACFBiasAdjMat(ResidFormingMat,ntp,ARO)
+% Bias adjustment for ACF of residuals. 
+
+    M_biasred   = zeros(ARO+1);
+    for i=1:(ARO+1)
+        Di                  = (diag(ones(1,ntp-i+1),i-1)+diag(ones(1,ntp-i+1),-i+1))/(1+(i==1));
+        for j=1:(ARO+1)
+           Dj               = (diag(ones(1,ntp-j+1),j-1)+diag(ones(1,ntp-j+1),-j+1))/(1+(j==1));
+           M_biasred(i,j)   = trace(ResidFormingMat*Di*ResidFormingMat*Dj)/(1+(i>1));
+        end
+    end
+    invM_biasred = inv(M_biasred);
+    
 end
 
 
