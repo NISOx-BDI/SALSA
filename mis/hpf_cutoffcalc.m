6b642627d35f799ab1b1598385934b1303aa02cb
asoroosh <srafyouni@gmail.com>
2020-05-21 14:39:41 +0100

    idk

--- mis/hpf_cutoffcalc.m
+++ mis/hpf_cutoffcalc.m
@@ -1,19 +1,19 @@
 function cutoff = hpf_cutoffcalc(X,TR,path2design)
-% Calculates the cutoff calc
+% Calculates the cutoff for the highpass filter, given the deisgn
 %
 % SA,Ox,2020
 %
-    [ffpathstr,ffname,ffext]=fileparts(path2design);
-    filename=[ffpathstr '/' ffname '.txt'];
-    fileID = fopen(filename,'w');
-    strpattern = [repmat('%f',1,size(X,2)) '\n'];
+    [ffpathstr,ffname,ffext] = fileparts(path2design);
+    filename                 = [ffpathstr '/' ffname '.txt'];
+    fileID                   = fopen(filename,'w');
+    strpattern               = [repmat('%f',1,size(X,2)) '\n'];
     fprintf(fileID,strpattern,X);
     fclose(fileID);
     
     call_fsl(['${FSLDIR}/bin/Text2Vest ' filename ' ' path2design]);
 
-    callcutoff = ['${FSLDIR}/bin/cutoffcalc -i ' path2design ' --tr=' num2str(TR)];
-    [status,cutoffstr] = call_fsl(callcutoff);
+    callcutoff          = ['${FSLDIR}/bin/cutoffcalc -i ' path2design ' --tr=' num2str(TR)];
+    [status,cutoffstr]  = call_fsl(callcutoff);
     
     if ~status
         cutoff = str2double(cutoffstr);
