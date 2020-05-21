1b561411ca8e4782b9394f4a77b97d47cc474c06
asoroosh <srafyouni@gmail.com>
2020-05-21 14:38:59 +0100

    two types of input is accepted

--- mis/ACLImage.m
+++ mis/ACLImage.m
@@ -1,9 +1,16 @@
-function ACL = ACLImage(Path2Img,Path2ACL)
+function ACL = ACLImage(V,Path2ACL)
+% V: should be either the path to an image or a 2-D matrix
+%    if 2-D matrix, should have form of VxT
 
-    [Y,ImgStat]   = CleanNIFTI_spm(Path2Img);
+if nargin==2
+    [Y,ImgStat]   = CleanNIFTI_spm(V);
     T = size(Y,2);
     acf = AC_fft(Y,T);
     ACL = sum(acf(:,1:round(T/4)).^2,2);
     CleanNIFTI_spm(ACL,'ImgInfo',ImgStat.spmV,'destdir',Path2ACL,'Removables',ImgStat.Removables);
 
+elseif nargin==1 && isnumeric(V)
+        T   = size(V,2); 
+        acf = AC_fft(V,T);
+        ACL = sum(acf(:,1:round(T/4)).^2,2);
 end
