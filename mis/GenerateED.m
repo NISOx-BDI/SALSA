249b320769694f37a7f940cd1b893a7ba8cfc305
asoroosh <srafyouni@gmail.com>
2020-05-26 12:56:08 +0100

    adds to the start and end

--- mis/GenerateED.m
+++ mis/GenerateED.m
@@ -1,15 +1,18 @@
 
-function EDX = GenerateED(lD,T,TR)
+function EDX = GenerateED(lD,T,TR,onset)
 % lD: length of stimuli in seconds
 % T : length of the scan
 % TR: TR in second
 
+    tonset      = onset.*2; % use the same onset for the end too.
+    Ted         = T-tonset;
     BCWidth     = fix(lD/2/TR);
+    
     oneSTIM     = [zeros(1, BCWidth), ones(1, BCWidth)];
     
-    numberSTIM  = fix(T/numel(oneSTIM));
+    numberSTIM  = fix(Ted/numel(oneSTIM));
     DD          = repmat(oneSTIM, [1, numberSTIM]);
-    
+    DD          = [zeros(1,onset) DD zeros(1,onset)];
     
     hrf         = spm_hrf(TR); 
     conv_dsgn   = conv(DD,hrf); 
