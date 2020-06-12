4fecfeebd17441d015a82f5e54ef377c4f01cfeb
asoroosh <srafyouni@gmail.com>
2020-06-12 11:31:30 +0100

    idk

--- mis/gfast.m
+++ mis/gfast.m
@@ -1,46 +1,46 @@
-% ts_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/filtered_func_data.nii.gz';
-% tcon_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/design.con';
-% dmat_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/design_mat.txt';
-% path2mask='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/mask.nii.gz';
-% parmat='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/mc/prefiltered_func_data_mcf.par';
-% %feat5/featlib.cc 
-% 
-% addpath('/Users/sorooshafyouni/Home/GitClone/FILM2/mis')
-% addpath('/Users/sorooshafyouni/Home/GitClone/FILM2/utils/Trend')
-% addpath('/Users/sorooshafyouni/Home/matlab/spm12')
-% 
-% [Y,ImgStat] = CleanNIFTI_spm(ts_fname,'demean');
-% Y = Y';
-% Y = Y - mean(Y);
-% T=900;
-% 
-% disp('MC params.')
-% MCp      = load(parmat); 
-% MCp      = GenMotionParam(MCp,24); 
-% X        = [load(dmat_fname) MCp];
-% 
-% X        = X-mean(X); 
-% 
-% disp('hpf')
-% K = hp_fsl(size(Y,1),100,0.645);    
-% X     = K*X;    % high pass filter the design
-% Y     = K*Y;  % high pass filter the data
-% 
-% X = [ones(T,1) X];
-% tcon     = zeros(1,size(X,2));
-% tcon(2)  = 1;
-% 
-% 
-% [cbhat,RES,stat,se,tv,zv,Wcbhat,WYhat,WRES,wse,wtv,wzv] = gfast5(Y,X,0.645,tcon); 
-% 
-% [PSDx,PSDy]   = DrawMeSpectrum(RES,1);
-% [WPSDx,WPSDy] = DrawMeSpectrum(WRES,1);
-% 
-% figure; hold on; grid on; 
-% plot(PSDx,mean(PSDy,2))
-% plot(WPSDx,mean(WPSDy,2))
+ts_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/filtered_func_data.nii.gz';
+tcon_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/design.con';
+dmat_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/design_mat.txt';
+path2mask='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/mask.nii.gz';
+parmat='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/mc/prefiltered_func_data_mcf.par';
+%feat5/featlib.cc 
 
-function [cbhat,RES,stat,se,tv,zv,Wcbhat,WYhat,WRES,WBLUSRES,wse,wtv,wzv,W,V,Cy] = gfast(Y,X,TR,tcon,pmethod)
+addpath('/Users/sorooshafyouni/Home/GitClone/FILM2/mis')
+addpath('/Users/sorooshafyouni/Home/GitClone/FILM2/utils/Trend')
+addpath('/Users/sorooshafyouni/Home/matlab/spm12')
+
+[Y,ImgStat] = CleanNIFTI_spm(ts_fname,'demean');
+Y = Y';
+Y = Y - mean(Y);
+T=900;
+
+disp('MC params.')
+MCp      = load(parmat); 
+MCp      = GenMotionParam(MCp,24); 
+X        = [load(dmat_fname) MCp];
+
+X        = X-mean(X); 
+
+disp('hpf')
+K = hp_fsl(size(Y,1),100,0.645);    
+X     = K*X;    % high pass filter the design
+Y     = K*Y;  % high pass filter the data
+
+X = [ones(T,1) X];
+tcon     = zeros(1,size(X,2));
+tcon(2)  = 1;
+
+
+[cbhat,RES,stat,se,tv,zv,Wcbhat,WYhat,WRES,wse,wtv,wzv] = gfast5(Y,X,0.645,tcon); 
+
+[PSDx,PSDy]   = DrawMeSpectrum(RES,1);
+[WPSDx,WPSDy] = DrawMeSpectrum(WRES,1);
+
+figure; hold on; grid on; 
+plot(PSDx,mean(PSDy,2))
+plot(WPSDx,mean(WPSDy,2))
+
+function [cbhat,RES,stat,se,tv,zv,Wcbhat,WYhat,WRES,WBLUSRES,wse,wtv,wzv,W,V,Cy] = gfast5(Y,X,TR,tcon,pmethod)
 %[W,V,Cy] = gfast(Y,X,TR)
 % 
 % Y:  Full time series TxV
@@ -128,15 +128,18 @@ function [cbhat,RES,stat,se,tv,zv,Wcbhat,WYhat,WRES,WBLUSRES,wse,wtv,wzv,W,V,Cy]
     % FAST variance components for FAST
     Vi      = spm_Ce('fast',ntp,TR);
 
+    tic; 
     % Call ReML to get the auto-covariance of the system
     disp('gfast:: Finding autocovariance matrix using ReML')
     V       = spm_reml(Cy,X,Vi);
     V       = V*ntp/trace(V); 
-
+    
     % Prewhitening Matrix, W
     disp('gfast:: getting global whitening matrix.')
     W      = spm_sqrtm(spm_inv(V));
     W      = W.*(abs(W)> 1e-6);
+    
+    toc
     
     % Prewhiten the X & Y globally 
     disp('gfast:: Whiten the data and the design')
