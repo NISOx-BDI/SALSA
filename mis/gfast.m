d333ba6557176872e805b65d78c70f387f96c1a3
asoroosh <srafyouni@gmail.com>
2020-06-29 11:24:30 +0100

    comments were fixed

--- mis/gfast.m
+++ mis/gfast.m
@@ -1,46 +1,46 @@
-ts_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/filtered_func_data.nii.gz';
-tcon_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/design.con';
-dmat_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/design_mat.txt';
-path2mask='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/mask.nii.gz';
-parmat='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/mc/prefiltered_func_data_mcf.par';
-%feat5/featlib.cc 
+% ts_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/filtered_func_data.nii.gz';
+% tcon_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/design.con';
+% dmat_fname='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/design_mat.txt';
+% path2mask='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/mask.nii.gz';
+% parmat='/Users/sorooshafyouni/Home/GitClone/FILM2/NullRealfMRI/FeatTest/sub-A00008326++++.feat/mc/prefiltered_func_data_mcf.par';
+% %feat5/featlib.cc 
+% 
+% addpath('/Users/sorooshafyouni/Home/GitClone/FILM2/mis')
+% addpath('/Users/sorooshafyouni/Home/GitClone/FILM2/utils/Trend')
+% addpath('/Users/sorooshafyouni/Home/matlab/spm12')
+% 
+% [Y,ImgStat] = CleanNIFTI_spm(ts_fname,'demean');
+% Y = Y';
+% Y = Y - mean(Y);
+% T=900;
+% 
+% disp('MC params.')
+% MCp      = load(parmat); 
+% MCp      = GenMotionParam(MCp,24); 
+% X        = [load(dmat_fname) MCp];
+% 
+% X        = X-mean(X); 
+% 
+% disp('hpf')
+% K = hp_fsl(size(Y,1),100,0.645);    
+% X     = K*X;    % high pass filter the design
+% Y     = K*Y;  % high pass filter the data
+% 
+% X = [ones(T,1) X];
+% tcon     = zeros(1,size(X,2));
+% tcon(2)  = 1;
+% 
+% 
+% [cbhat,RES,stat,se,tv,zv,Wcbhat,WYhat,WRES,wse,wtv,wzv] = gfast5(Y,X,0.645,tcon); 
+% 
+% [PSDx,PSDy]   = DrawMeSpectrum(RES,1);
+% [WPSDx,WPSDy] = DrawMeSpectrum(WRES,1);
+% 
+% figure; hold on; grid on; 
+% plot(PSDx,mean(PSDy,2))
+% plot(WPSDx,mean(WPSDy,2))
 
-addpath('/Users/sorooshafyouni/Home/GitClone/FILM2/mis')
-addpath('/Users/sorooshafyouni/Home/GitClone/FILM2/utils/Trend')
-addpath('/Users/sorooshafyouni/Home/matlab/spm12')
-
-[Y,ImgStat] = CleanNIFTI_spm(ts_fname,'demean');
-Y = Y';
-Y = Y - mean(Y);
-T=900;
-
-disp('MC params.')
-MCp      = load(parmat); 
-MCp      = GenMotionParam(MCp,24); 
-X        = [load(dmat_fname) MCp];
-
-X        = X-mean(X); 
-
-disp('hpf')
-K = hp_fsl(size(Y,1),100,0.645);    
-X     = K*X;    % high pass filter the design
-Y     = K*Y;  % high pass filter the data
-
-X = [ones(T,1) X];
-tcon     = zeros(1,size(X,2));
-tcon(2)  = 1;
-
-
-[cbhat,RES,stat,se,tv,zv,Wcbhat,WYhat,WRES,wse,wtv,wzv] = gfast5(Y,X,0.645,tcon); 
-
-[PSDx,PSDy]   = DrawMeSpectrum(RES,1);
-[WPSDx,WPSDy] = DrawMeSpectrum(WRES,1);
-
-figure; hold on; grid on; 
-plot(PSDx,mean(PSDy,2))
-plot(WPSDx,mean(WPSDy,2))
-
-function [cbhat,RES,stat,se,tv,zv,Wcbhat,WYhat,WRES,WBLUSRES,wse,wtv,wzv,W,V,Cy] = gfast5(Y,X,TR,tcon,pmethod)
+function [cbhat,RES,stat,se,tv,zv,Wcbhat,WYhat,WRES,WBLUSRES,wse,wtv,wzv,W,V,Cy] = gfast(Y,X,TR,tcon,pmethod)
 %[W,V,Cy] = gfast(Y,X,TR)
 % 
 % Y:  Full time series TxV
