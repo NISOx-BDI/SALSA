6b0b2213750d61e0c4c2733cbb9a779be2b95071
asoroosh <srafyouni@gmail.com>
2020-09-22 14:44:00 +0100

    runs on OpenMP

--- NullRealfMRI/Img/Submit_NullImgSim_AFNI.sh
+++ NullRealfMRI/Img/Submit_NullImgSim_AFNI.sh
@@ -1,35 +1,122 @@
 #bin/bash
+
 set -e
 
-AFNI_SPM_singularity_image=/apps/singularity/afni-r-python3-2020-03-26-v1.sif
-AFNI_bin=/opt/afni-latest
+# COHORT      T    TR
+# Cambridge  119   3
+# Beijing    225   2
+# ROCKLAND   900   0.645
+# ROCKLAND   120   2.5
+# ROCKLAND   404   1.4
+# HCP        1200  0.72
+# NEO	     2300  0.392
 
-BOLDIMG=${path_data}/${subject}_${task}_bold.nii
-stim_times=
-HRF=
-smoothing=
-zero_aux=
-TR=
+#COHORT=ROCKLAND
+#T=404 #120
+#TRs=1.4 #2.5 # in second
+#NSUB=51
+#EDtype="ER"
 
-singularity exec --cleanenv -B $home_dir $AFNI_SPM_singularity_image \
-$AFNI_bin/afni_proc.py 							 \
--subj_id $subject                                                        \
--script proc.$subject -scr_overwrite                                     \
--regress_opts_3dD -force_TR $TR                                          \
--tcat_remove_first_trs 0                                                 \
--dsets ${BOLDIMG}			                                 \
--volreg_align_to third                                                   \
--regress_polort 2                                                        \
--regress_bandpass 0.01 10                                                \
--blur_size ${smoothing}.${zero_aux}                                      \
--regress_stim_types AM1                                                  \
--regress_stim_times $stim_times                                          \
--regress_stim_labels activation_stimulus                                 \
--regress_basis ${HRF}                                                    \
--regress_make_ideal_sum sum_ideal.1D                                     \
--regress_run_clustsim no                                                 \
--regress_est_blur_epits                                                  \
--regress_est_blur_errts                                                  \
--regress_reml_exec                                                       \
--regress_opts_reml -Rwherr whitened_errts.${subject}_REML
+#COHORT=Beijing
+#T=225 #120
+#TRs=2 #2.5 # in second
+#NSUB=51
+#EDtype="ER"
+
+COHORT=ROCKLAND
+T=900 #120
+TRs=0.645 #2.5 # in second
+NSUB=51
+EDtype="ER"
+
+#COHORT=NEO
+#T=2300 #120
+#TRs=0.392 #2.5 # in second
+#NSUB=51
+#EDtype="ER"
+
+#COHORT=HCP
+#T=1200 #120
+#TRs=0.72 #2.5 # in second
+#NSUB=51
+#EDtype="ER"
+
+######################################
+
+GSRFLAG=0
+ICAFLAG=0
+
+FWHMsize=5
+
+QSUBFLAG=1
+
+ARO=1
+MAO=1
+TempTreMethod=poly
+METH_ID=3dREMLfit
+
+TR=$(echo $TRs*1000 | bc | awk -F'.' {'print $1'})
+############################################
+if [ $COHORT == "Beijing" ] || [ $COHORT == "ROCKLAND" ]; then
+	NCORES=2
+elif [ $COHORT == "HCP" ] || [ $COHORT == "NEO" ] ; then
+	NCORES=4
+fi
+############################################
+STRGDIR=/well/nichols/users/scf915
+COHORTDIR=${STRGDIR}/${COHORT}
+COHORTPWDIR=${COHORTDIR}
+SUBMITDIR=/users/nichols/scf915/bin/FILM2/NullRealfMRI/Img/${COHORT}_${T}_${TR}_gsr${GSRFLAG}_aroma${ICAFLAG}_Submitters
+mkdir -p ${SUBMITDIR}
+JobName=${COHORT}_${TR}_${T}_${METH_ID}_AR-${ARO}_MA-${MAO}_FWHM${FWHMsize}_${TempTreMethod}_gsr${GSRFLAG}_aroma${ICAFLAG}
+SubmitterFileName="${SUBMITDIR}/SubmitMe_${JobName}.sh"
+echo $SubmitterFileName
+Path2ImgResults=${COHORTPWDIR}/R.PW/${JobName}
+OpLog=${Path2ImgResults}/logs
+
+############################################
+############################################
+cat > $SubmitterFileName << EOF
+#!/bin/bash
+#$ -cwd
+#$ -q short.qe # short.qc@@short.hge #short.qe    # short.qc@@short.hge
+#$ -pe shmem ${NCORES}
+#$ -o ${OpLog}/${JobName}_\\\$JOB_ID_\\\$TASK_ID.out
+#$ -e ${OpLog}/${JobName}_\\\$JOB_ID_\\\$TASK_ID.err
+#$ -N ${JobName}
+#$ -t 1-${NSUB} #${NUMJB}
+
+export OMP_NUM_THREADS=${NCORES}
+
+STATFILE=${OpLog}/${JobName}_\${JOB_ID}_\${SGE_TASK_ID}.stat
+
+# The stat file
+echo 0 > \$STATFILE
+
+# This whole business is rubbish! This should be fixed!
+source \${HOME}/.bashrc
+# module use -a /apps/eb/skylake/modules/all
+module load Octave/4.4.1-foss-2018b
+module load fsl/6.0.3
+
+#SubID=\$(cat ${COHORTDIR}/sub.txt | sed "\${SGE_TASK_ID}q;d" )
+SubID=\$(cat ${COHORTPWDIR}/${COHORT}_subid.txt | awk {'print \$1'} | sed "\${SGE_TASK_ID}q;d")
+SesID=\$(cat ${COHORTPWDIR}/${COHORT}_sesid.txt | awk {'print \$1'} | sed "\${SGE_TASK_ID}q;d")
+#SubID=\$(echo \${SubID} | awk -F"-" '{print \$2}')
+
+# Run 3dREMLfit -----
+#COHORT=$1 TRs=$2 T=$3 SubID=$4 SesID=$5 FWHMsize=$6 GSRFLAG=$7 ICAFLAG=$8
+sh /users/nichols/scf915/bin/FILM2/mis/run_Null3dREMLfit.sh $COHORT $TRs $T \$SubID \$SesID $FWHMsize $GSRFLAG $ICAFLAG $Path2ImgResults
+
+# The stat file
+echo 1 > \$STATFILE
+
+EOF
+
+############################################
+############################################
+
+if [ $QSUBFLAG == 1 ]; then
+	qsub $SubmitterFileName
+fi
 
