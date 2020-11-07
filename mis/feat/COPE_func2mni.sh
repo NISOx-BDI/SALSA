ffb2bee1ce3edcc4aa68416f57fbd4bf1f785986
asoroosh <srafyouni@gmail.com>
2020-11-07 16:11:24 +0000

    idk

--- mis/feat/COPE_func2mni.sh
+++ mis/feat/COPE_func2mni.sh
@@ -2,18 +2,19 @@
 
 set -e
 
-#COHORT=ROCKLAND
-#T=240
-#TRs=0.645
-#TaskName=CHECKERBOARD
-#NSUB=25
-####################################
-COHORT=tHCP
-T=284
-TRs=0.72
-TaskName=MOTOR
+COHORT=ROCKLAND
+T=240
+TRs=0.645
+TaskName=CHECKERBOARD
 NSUB=25
-StimulName="_lh"
+StimulName=""
+####################################
+#COHORT=tHCP
+#T=284
+#TRs=0.72
+#TaskName=MOTOR
+#NSUB=25
+#StimulName="_lh"
 ###################################
 #COHORT=tHCP
 #T=253
@@ -37,7 +38,7 @@ EDtype_Rmpp="task-${TaskName}_acq-${TR}"
 
 #METHODLIST=(ACF gFAST gACFadjxACFadj2tT1S0P5 AR-W)
 
-METHODLIST=(ACF)
+METHODLIST=(3dREMLfit)
 
 ARMODE=(1 2 5 10 20)
 
@@ -56,6 +57,19 @@ else
 #	exit 1
 fi
 
+if [ ${#METHODLIST[@]} == 1 ] & [ $METHODLIST == '3dREMLfit' ]; then
+	TempTreMethodLIST=(poly)
+	TempTrendP=2
+#	if [ $COHORT == "ROCKLAND" ] && [ $T == "240" ] && [ $TaskName == "CHECKERBOARD" ]; then
+#        	echo ${COHORT}, ${T}
+#        	TempTrendP=(2)
+#	elif [ $COHORT == "tHCP" ] && [ $TaskName == "MOTOR" ]; then
+#        	TempTrendP=(2)
+#	elif [ $COHORT == "tHCP" ] && [ $TaskName == "GAMBLING" ]; then
+#        	TempTrendP=(2)
+fi
+
+
 tr_cnt=0
 for TempTreMethod in ${TempTreMethodLIST[@]};do
         echo $tr_cnt
@@ -66,11 +80,14 @@ for TempTreMethod in ${TempTreMethodLIST[@]};do
                         ARMODE0=${ACMODE[@]}
                 elif [ $METH_ID == "gFAST" ]; then
                         ARMODE0=1
+		elif [ $METH_ID == "3dREMLfit" ]; then
+			ARMODE0=1
+			MAO=1
                 fi
 
                 for ARO in ${ARMODE0[@]}; do
 
-			ConDir="/well/nichols/users/scf915/${COHORT}/R.PW/${COHORT}_${TR}_${T}_${TaskName}_${METH_ID}_AR-${ARO}_MA-0_FWHM${FWHMsize}_${TempTreMethod}_gsr0_aroma0"
+			ConDir="/well/nichols/users/scf915/${COHORT}/R.PW/${COHORT}_${TR}_${T}_${TaskName}_${METH_ID}_AR-${ARO}_MA-${MAO}_FWHM${FWHMsize}_${TempTreMethod}_gsr0_aroma0"
                         for (( s=1; s<=${NSUB}; s++ ))
                         do
                                         SubID=$(cat ${COHORTDIR}/${COHORT}_subid.txt | awk {'print $1'} | sed "${s}q;d")
@@ -82,15 +99,26 @@ for TempTreMethod in ${TempTreMethodLIST[@]};do
 
 
                                         SubDIR="${ConDir}/${SubID}_${SesID}"
-                                        TDIR=${SubDIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA0_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_cbhat_ICACLEAN0_GSR0.nii.gz
-					wTDIR=${SubDIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA0_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_Wcbhat_ICACLEAN0_GSR0.nii.gz
 
-					wVDIR=${SubDIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA0_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_wvc_ICACLEAN0_GSR0.nii.gz
+                                        TDIR=${SubDIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA${MAO}_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_cbhat_ICACLEAN0_GSR0.nii.gz
+					wTDIR=${SubDIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA${MAO}_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_Wcbhat_ICACLEAN0_GSR0.nii.gz
+					wVDIR=${SubDIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA${MAO}_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_wvc_ICACLEAN0_GSR0.nii.gz
 
-                                        if [ ! -f $TDIR ] || [ ! -f $wTDIR ] || [ ! -f $wVDIR ]; then
+					if [ $METH_ID == '3dREMLfit' ]; then
+						echo "copy stuff to the right naming format."
+						WBETAtmp=${SubDIR}/${EDtype}_sub-${SubID}_ses-${SesID}_Decon_wbeta.nii.gz
+						WSTDtmp=${SubDIR}/${EDtype}_sub-${SubID}_ses-${SesID}_Decon_REMLvar_wstd.nii.gz
+
+						${FSLDIR}/bin/fslmaths ${WBETAtmp} ${wTDIR} #just copy
+						${FSLDIR}/bin/fslmaths ${WSTDtmp} -sqr ${wVDIR} # AFNI outputs std, we need variance
+ 					fi
+
+#                                        if [ ! -f $TDIR ] || [ ! -f $wTDIR ] || [ ! -f $wVDIR ]; then
+					if  [ ! -f $wTDIR ] || [ ! -f $wVDIR ]; then
                                                 echo "$TDIR does not exists."
                                                 continue 1
                                         fi
+
 					if [ $COHORT == "ROCKLAND" ]; then
 						InDir="/well/nichols/users/scf915/${COHORT}/R_mpp/sub-${SubID}/ses-${SesID}/sub-${SubID}_ses-${SesID}_${EDtype_Rmpp}_bold_mpp"
 					elif [ $COHORT == "tHCP" ]; then
@@ -98,7 +126,7 @@ for TempTreMethod in ${TempTreMethodLIST[@]};do
 					fi
 #					echo $InDir
 
-					sh func2mni.sh $TDIR $InDir
+#					sh func2mni.sh $TDIR $InDir # We don't really need the none-whitened case
 					sh func2mni.sh $wTDIR $InDir
 					sh func2mni.sh $wVDIR $InDir
 
@@ -111,20 +139,22 @@ for TempTreMethod in ${TempTreMethodLIST[@]};do
 
 			mkdir -p ${LEVEL2DIR}
 
-			${FSLDIR}/bin/fslmerge \
-			-t  ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA0_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_cbhat_4D \
-			${ConDir}/*_*/mni/*_cbhat_ICACLEAN0_GSR0_mni.nii.gz
-			echo "4D data is: ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA0_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_cbhat_4D"
+
+			# We don't really need the non-whitened case! AND even if we need, only a loop around detrending methods should be enough.
+			#${FSLDIR}/bin/fslmerge \
+			#-t  ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA${MAO}_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_cbhat_4D \
+			#${ConDir}/*_*/mni/*_cbhat_ICACLEAN0_GSR0_mni.nii.gz
+			#echo "4D data is: ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA${MAO}_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_cbhat_4D"
 
 			${FSLDIR}/bin/fslmerge \
-			-t  ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA0_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_Wcbhat_4D \
+			-t  ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA${MAO}_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_Wcbhat_4D \
 			${ConDir}/*_*/mni/*_Wcbhat_ICACLEAN0_GSR0_mni.nii.gz
-                        echo "4D data is: ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA0_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_Wcbhat_4D"
+                        echo "4D data is: ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA${MAO}_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_Wcbhat_4D"
 
                         ${FSLDIR}/bin/fslmerge \
-                        -t  ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA0_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_wvc_4D \
+                        -t  ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA${MAO}_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_wvc_4D \
                         ${ConDir}/*_*/mni/*_wvc_ICACLEAN0_GSR0_mni.nii.gz
-                        echo "4D data is: ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA0_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_wvc_4D"
+                        echo "4D data is: ${LEVEL2DIR}/ED${EDtype}_${METH_ID}_AR${ARO}_MA${MAO}_FWHM${FWHMsize}_${TempTreMethod}${TempTrendP[$tr_cnt]}_wvc_4D"
 
 
 
